8e81476410658f386ab9e3a2a06c760283224e56
Matteo Manchi <matteo.manchi@gmail.com>
2014-06-18 12:33:23 +0200

    EnergyDetectorApp offlineCalculations

--- src/it/uniroma3/signals/EnergyDetectorApp.java
+++ src/it/uniroma3/signals/EnergyDetectorApp.java
@@ -1,9 +1,43 @@
 package it.uniroma3.signals;
 
-public class EnergyDetectorApp {
+import it.uniroma3.signals.spectrumsensing.energydetector.EnergyDetector;
 
-	public static void main(String[] args) {
+import java.util.Scanner;
+
+public class EnergyDetectorApp {
+	private Scanner in;
+	private EnergyDetector ed;
+	
+	public EnergyDetectorApp() {
+		this.in = new Scanner(System.in);
+	}
+	
+	public void offlineCalculations() {
+		System.out.println("############ Offline Section ############");
+		System.out.print("Inserire il SNR (in db) al quale si vuole lavorare: ");
+		this.ed = new EnergyDetector(this.in.nextDouble());
 		
+		System.out.print("Inserire la probabilità di falso allarme desiderata: ");
+		double Pfa = this.in.nextDouble();
+		
+		System.out.print("Inserire il numero di tentativi da effettuare per il calcolo della soglia: ");
+		int attempts = this.in.nextInt();
+		
+		ed.generateThreshold(attempts, Pfa);
+		
+		System.out.println("Lavorando offline e' stata generato un valore di soglia per il dato SNR di: " + ed.getThreshold());
+	}
+	
+	public void onlineDetection() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		EnergyDetectorApp app = new EnergyDetectorApp();
+		
+		app.offlineCalculations();
+		app.onlineDetection();
 	}
 
 }
